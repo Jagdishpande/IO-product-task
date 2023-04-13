b79bd86f8ffee351b7cfb576ed9dc018dcb71060
Jagdish Pande <122719486+8668577812@users.noreply.github.com>
2023-04-13 18:24:17 +0530

    product setup task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductCatg, Iproducts } from './shared/model/products';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iOProducts';
+
+  productArray:Array<Iproducts>=[
+    {
+      productName:"Samsung",
+      pDescription:"lorem ipsum is the dummy paragraph we use it",
+      pCategory:"Catlog"
+    },
+    {
+      productName:"Samsung",
+      pDescription:"lorem ipsum is the dummy paragraph we use it",
+      pCategory:"Product"
+    }
+  ]
+
+  onAddProductDetails(pname:HTMLInputElement,pDescri:HTMLTextAreaElement,catg:IProductCatg){
+    let obj:Iproducts={
+      productName:pname.value,
+      pDescription:pDescri.value,
+      pCategory:catg
+    }
+    this.productArray.push(obj)
+    pname.value="",
+    pDescri.value=""
+  }
 }
---
+++ src/app/shared/model/products.ts
@@ -0,0 +1,6 @@
+export interface Iproducts{
+    productName:string;
+    pDescription:string;
+    pCategory:"Catlog"|"Product"
+}
+export type IProductCatg="Catlog"|"Product";
